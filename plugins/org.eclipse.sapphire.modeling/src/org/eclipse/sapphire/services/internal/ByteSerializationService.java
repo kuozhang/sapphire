d2c15a8030ccb463dd36d81ffb807aca2f844e63
kkomissarchik <kkomissarchik>
2011-08-23 23:25:41 +0000

    Bug 355581 - Why not offer a byte and short serialization service?

---
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/internal/ByteSerializationService.java
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * Copyright (c) 2011 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
+package org.eclipse.sapphire.services.internal;
+
+import org.eclipse.sapphire.modeling.ValueProperty;
+import org.eclipse.sapphire.services.Service;
+import org.eclipse.sapphire.services.ServiceContext;
+import org.eclipse.sapphire.services.ServiceFactory;
+import org.eclipse.sapphire.services.ValueSerializationService;
+
+/**
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
+
+public final class ByteSerializationService extends ValueSerializationService
+{
+    @Override
+    protected Byte decodeFromString( final String value )
+    {
+        Byte result = null;
+        
+        try
+        {
+            result = Byte.valueOf( value );
+        }
+        catch( NumberFormatException e )
+        {
+            // Intentionally ignored. It is not the job of serializer to report these
+            // problems. That's handled by validators.
+        }
+        
+        return result;
+    }
+    
+    public static final class Factory extends ServiceFactory
+    {
+        @Override
+        public boolean applicable( final ServiceContext context,
+                                   final Class<? extends Service> service )
+        {
+            final ValueProperty property = context.find( ValueProperty.class ); 
+            return ( property != null && property.isOfType( Byte.class ) );
+        }
+    
+        @Override
+        public Service create( final ServiceContext context,
+                               final Class<? extends Service> service )
+        {
+            return new ByteSerializationService();
+        }
+    }
+
+}
