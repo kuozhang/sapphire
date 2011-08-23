68f4a7df43c54ed15b8413ebfb25351f7b5a68c8
kkomissarchik <kkomissarchik>
2011-08-23 23:21:44 +0000

    Bug 355581 - Why not offer a byte and short serialization service?

---
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/modeling/serialization/internal/ByteSerializationService.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Copyright (c) 2011 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
+package org.eclipse.sapphire.modeling.serialization.internal;
+
+import org.eclipse.sapphire.modeling.serialization.ValueSerializationService;
+
+/**
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
+
+public final class ByteSerializationService extends ValueSerializationService
+{
+    @Override
+    protected Byte decodeFromString( final String value )
+    {
+        Byte result = null;
+        
+        try
+        {
+            result = Byte.valueOf( value );
+        }
+        catch( NumberFormatException e )
+        {
+            // Intentionally ignored. It is not the job of serializer to report these
+            // problems. That's handled by validators.
+        }
+        
+        return result;
+    }
+    
+}
