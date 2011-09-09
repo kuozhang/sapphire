81b550f015221c073ed9721da6c9784fa6b8c79d
kkomissarchik <kkomissarchik>
2011-09-09 18:29:34 +0000

    Copyright header fix.

--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/diagram/def/IDiagramPaletteCompartmentDef.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/diagram/def/IDiagramPaletteCompartmentDef.java
@@ -5,6 +5,8 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Contributors:
+ *    Shenxue Zhou - initial implementation and ongoing maintenance
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.diagram.def;
