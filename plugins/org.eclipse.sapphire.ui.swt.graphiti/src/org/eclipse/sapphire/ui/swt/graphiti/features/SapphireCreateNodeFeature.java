a39465f5c30c7a9bfed02e8ea8757a03bc805cf4
szhou <szhou>
2011-09-14 23:09:29 +0000

    Fix another typo which could lead to NPE.

--- plugins/org.eclipse.sapphire.ui.swt.graphiti/src/org/eclipse/sapphire/ui/swt/graphiti/features/SapphireCreateNodeFeature.java
+++ plugins/org.eclipse.sapphire.ui.swt.graphiti/src/org/eclipse/sapphire/ui/swt/graphiti/features/SapphireCreateNodeFeature.java
@@ -79,9 +79,9 @@ public class SapphireCreateNodeFeature extends AbstractCreateFeature implements 
 		{
 			otherName = ((SapphireCreateNodeFeature)o).getCreateName();
 		}
-		else if (o instanceof SapphireCreateNodeFeature)
+		else if (o instanceof SapphireCreateConnectionFeature)
 		{
-			otherName = ((SapphireCreateNodeFeature)o).getCreateName();
+			otherName = ((SapphireCreateConnectionFeature)o).getCreateName();
 		}
 		return createName.compareTo(otherName);
 	}
