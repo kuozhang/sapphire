c7b3d7820aa484ec49b99ed3cd1cf27c4eb68302
szhou <szhou>
2011-09-26 21:26:24 +0000

    Bug 358973 - Sapphire diagram should not persist Graphiti's EMF based diagram model. Use a "virtual" in-memory uri to store the diagram model

--- plugins/org.eclipse.sapphire.ui.swt.graphiti/src/org/eclipse/sapphire/ui/swt/graphiti/editor/SapphireDiagramEditorFactory.java
+++ plugins/org.eclipse.sapphire.ui.swt.graphiti/src/org/eclipse/sapphire/ui/swt/graphiti/editor/SapphireDiagramEditorFactory.java
@@ -13,8 +13,6 @@
 package org.eclipse.sapphire.ui.swt.graphiti.editor;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -22,21 +20,16 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.editor.DiagramEditorFactory;
-import org.eclipse.graphiti.ui.internal.services.GraphitiUiInternal;
 import org.eclipse.graphiti.ui.services.GraphitiUi;
 import org.eclipse.sapphire.modeling.StatusException;
 import org.eclipse.sapphire.modeling.util.internal.FileUtil;
-import org.eclipse.sapphire.ui.internal.SapphireUiFrameworkPlugin;
-import org.eclipse.sapphire.ui.swt.graphiti.GraphitiFileService;
 
 /**
  * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
@@ -106,72 +99,33 @@ public class SapphireDiagramEditorFactory
             existingLayout = false;
         }
         
-        // We don't need to persist Graphiti diagram. But due to limitations on Graphiti's
-        // diagram model, we still need to pass a file when creating Graphiti Diagram obj.
-        // See http://www.eclipse.org/forums/index.php/t/202467/
-        
-        TransactionalEditingDomain domain = null;
-        ResourceSet resourceSet = null;
-        Diagram diagram = null;
-        URI diagramFileUri = null;
+    	final Diagram diagram = Graphiti.getPeCreateService().createDiagram(SAPPHIRE_DIAGRAM_TYPE, fileName, 10, true);
+    	// Create a virtual URI with a custom schema
+    	URI uri = URI.createHierarchicalURI("Virtual",
+    			null,
+    			null,
+    			new String[] { "diagram", fileName },
+    			null,
+    			null);
 
-        String diagramFileName = fileName + ".xmi";
-        IFile diagramFile = diagramSettingFolder.getFile(diagramFileName);
+    	// Create a regular resource in the uri
+    	TransactionalEditingDomain editingDomain = DiagramEditorFactory.createResourceSetAndEditingDomain();
+    	final Resource diagramResource = editingDomain.getResourceSet().createResource(uri);				
 
-        if (!diagramFile.exists())
-        {
-            diagramFile.create(new ByteArrayInputStream(new byte[0]), true, null);            
-            
-            // Create Diagram Obj
-            diagram = Graphiti.getPeCreateService().createDiagram(
-                    SAPPHIRE_DIAGRAM_TYPE, fileName, 10, true);
-            diagramFileUri = URI.createPlatformResourceURI(diagramFile.getFullPath().toString(), true);
-            domain = GraphitiFileService.createEmfFileForDiagram(diagramFileUri, diagram);
-        }
-        else
-        {            
-            domain = DiagramEditorFactory.createResourceSetAndEditingDomain();
-            resourceSet = domain.getResourceSet();
-        
-            diagramFileUri = GraphitiUiInternal.getEmfService().getFileURI(diagramFile, resourceSet);
-            if (diagramFileUri != null)
-            {
-                final Resource resource = resourceSet.createResource(diagramFileUri);
-                try
-                {
-                    resource.load(null);
-                }
-                catch (IOException ie)
-                {
-                    SapphireUiFrameworkPlugin.log(ie);
-                }
-                EList<EObject> objs = resource.getContents();
-                Iterator<EObject> it = objs.iterator();                    
-                while (it.hasNext()) 
-                {
-                    EObject obj = it.next();
-                    if ((obj == null) && !Diagram.class.isInstance(obj))
-                        continue;
-                    diagram = (Diagram)obj;
-                    break;
-                }
-            }                
-        }
-
-        // create sapphire diagram editor input
-        if (diagram != null)
-        {
-            String providerId = GraphitiUi.getExtensionManager().getDiagramTypeProviderId(diagram.getDiagramTypeId());
-            final SapphireDiagramEditorInput diagramEditorInput = 
-                SapphireDiagramEditorInput.createEditorInput(diagram, domain, providerId, false);
-            diagramEditorInput.setLayoutFile(layoutFile);
-            diagramEditorInput.setNoExistingLayout(!existingLayout);
-            return diagramEditorInput;
-        }
-        else
-        {
-            return null;
-        }
+    	// Add the diagram to the resource
+    	editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) 
+		{
+			protected void doExecute() 
+			{			    					
+				diagramResource.getContents().add(diagram);
+			}
+		});
+    	String providerId = GraphitiUi.getExtensionManager().getDiagramTypeProviderId(diagram.getDiagramTypeId());
+        final SapphireDiagramEditorInput diagramEditorInput = 
+            SapphireDiagramEditorInput.createEditorInput(diagram, editingDomain, providerId, false);
+        diagramEditorInput.setLayoutFile(layoutFile);
+        diagramEditorInput.setNoExistingLayout(!existingLayout);
+        return diagramEditorInput;        	
     }
-
+    
 }
