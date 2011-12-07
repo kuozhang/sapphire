213d0905d121aac340c7d38a5437332a15bd3113
lhao <lhao>
2011-12-07 20:31:34 +0000

    add missing copyright

--- plugins/org.eclipse.sapphire.ui.gef.diagram.editor/src/org/eclipse/sapphire/ui/gef/diagram/editor/model/DiagramImageCache.java
+++ plugins/org.eclipse.sapphire.ui.gef.diagram.editor/src/org/eclipse/sapphire/ui/gef/diagram/editor/model/DiagramImageCache.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2011 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ling Hao - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
 package org.eclipse.sapphire.ui.gef.diagram.editor.model;
 
 import java.net.URL;
@@ -15,6 +26,10 @@ import org.eclipse.sapphire.ui.diagram.def.IDiagramNodeImageDef;
 import org.eclipse.sapphire.ui.diagram.editor.DiagramNodeTemplate;
 import org.eclipse.sapphire.ui.diagram.editor.SapphireDiagramEditorPagePart;
 import org.osgi.framework.Bundle;
+
+/**
+ * @author <a href="mailto:ling.hao@oracle.com">Ling Hao</a>
+ */
 
 public class DiagramImageCache {
 
