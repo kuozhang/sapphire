58d5e85ffc21c27ef579db108cdf4a20cc87a44d
kkomissarchik <kkomissarchik>
2012-02-14 00:24:19 +0000

    Updates to copyright statements.

--- plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/ContentProposal.java
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/ContentProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Oracle and Accenture Services Pvt Ltd.
+ * Copyright (c) 2012 Oracle and Accenture Services Pvt Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
