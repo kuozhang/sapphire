02689a99fb3107e860ed90f39d3541bd383c9b57
kkomissarchik <kkomissarchik>
2012-04-02 15:46:03 +0000

    Bug 374821 - Support group with no label

--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/SapphireGroup.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/SapphireGroup.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2012 Oracle
+ * Copyright (c) 2012 Oracle and Modelity Technologies
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ *    Roded Bahat - [374821] Support group with no label
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui;
@@ -19,6 +20,7 @@ import static org.eclipse.sapphire.ui.swt.renderer.GridLayoutUtil.glayout;
 
 import org.eclipse.sapphire.modeling.CapitalizationType;
 import org.eclipse.sapphire.modeling.localization.LabelTransformer;
+import org.eclipse.sapphire.modeling.util.MiscUtil;
 import org.eclipse.sapphire.ui.def.ISapphireGroupDef;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -26,19 +28,22 @@ import org.eclipse.swt.widgets.Group;
 
 /**
  * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ * @author <a href="mailto:rodedb@gmail.com">Roded Bahat</a>
  */
 
-public class SapphireGroup
-
-    extends SapphireComposite
-    
+public class SapphireGroup extends SapphireComposite
 {
     @Override
     protected Composite createOuterComposite( final SapphireRenderingContext context )
     {
         final ISapphireGroupDef def = (ISapphireGroupDef) this.definition;
         
-        final String label = LabelTransformer.transform( def.getLabel().getLocalizedText(), CapitalizationType.FIRST_WORD_ONLY, true );
+        String label = MiscUtil.normalizeToEmptyString( def.getLabel().getLocalizedText() );
+        
+        if( label.length() != 0 ) 
+        {
+            label = LabelTransformer.transform( label, CapitalizationType.FIRST_WORD_ONLY, true );
+        }
         
         final Group group = new Group( context.getComposite(), SWT.NONE );
         group.setLayoutData( gdhindent( gdvindent( gdhspan( gdhfill(), 2 ), 5 ), 10 ) );
--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/def/ISapphireGroupDef.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/def/ISapphireGroupDef.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2012 Oracle
+ * Copyright (c) 2012 Oracle and Modelity Technologies
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ *    Roded Bahat - [374821] Support group with no label
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.def;
@@ -16,28 +17,24 @@ import org.eclipse.sapphire.modeling.Value;
 import org.eclipse.sapphire.modeling.ValueProperty;
 import org.eclipse.sapphire.modeling.annotations.GenerateImpl;
 import org.eclipse.sapphire.modeling.annotations.Label;
-import org.eclipse.sapphire.modeling.annotations.Required;
 import org.eclipse.sapphire.modeling.localization.Localizable;
 import org.eclipse.sapphire.modeling.xml.annotations.XmlBinding;
 
 /**
  * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ * @author <a href="mailto:rodedb@gmail.com">Roded Bahat</a>
  */
 
 @Label( standard = "group" )
 @GenerateImpl
 
-public interface ISapphireGroupDef
-
-    extends ISapphireCompositeDef
-    
+public interface ISapphireGroupDef extends ISapphireCompositeDef
 {
     ModelElementType TYPE = new ModelElementType( ISapphireGroupDef.class );
  
     // *** Label ***
     
     @Label( standard = "label" )
-    @Required
     @Localizable
     @XmlBinding( path = "label" )
     
