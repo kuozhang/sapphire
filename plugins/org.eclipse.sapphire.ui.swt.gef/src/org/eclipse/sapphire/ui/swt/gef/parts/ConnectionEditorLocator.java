9ed451ddf4a52f2aa05689a427148bdd27eb8272
lhao <lhao>
2012-06-20 05:13:52 +0000

    Bug 382651 - New connection label editing should scroll into view

--- plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/parts/ConnectionEditorLocator.java
+++ plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/parts/ConnectionEditorLocator.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.sapphire.ui.swt.gef.parts;
 
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.tools.CellEditorLocator;
@@ -23,7 +24,9 @@ import org.eclipse.swt.widgets.Text;
  */
 
 public class ConnectionEditorLocator implements CellEditorLocator {
+	
 	private Label label;
+	private DiagramConnectionLabelEditPart source;
 
 	/**
 	 * Creates a new ActivityCellEditorLocator for the given Label
@@ -31,14 +34,21 @@ public class ConnectionEditorLocator implements CellEditorLocator {
 	 * @param label
 	 *            the Label
 	 */
-	public ConnectionEditorLocator(Label label) {
+	public ConnectionEditorLocator(DiagramConnectionLabelEditPart source, Label label) {
 		setLabel(label);
+		this.source = source;
 	}
 
 	/**
 	 * @see CellEditorLocator#relocate(org.eclipse.jface.viewers.CellEditor)
 	 */
 	public void relocate(CellEditor celleditor) {
+		IFigure sourceFigure = source.getFigure();
+		Rectangle exposeRegion = sourceFigure.getBounds().getCopy();
+		if (exposeRegion.width > 0 && exposeRegion.height > 0) {
+			source.getViewer().reveal(source);
+		}
+		
 		Text text = (Text) celleditor.getControl();
 		Point pref = text.computeSize(-1, -1);
 		Rectangle rect = label.getTextBounds().getCopy();
@@ -57,6 +67,7 @@ public class ConnectionEditorLocator implements CellEditorLocator {
 
 	/**
 	 * Sets the label.
+	 * 
 	 * 
 	 * @param label
 	 *            The label to set
