40ef1e68f36874a2632a42f59210f860f67eeb0e
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2012-08-02 10:45:45 -0700

    Temporarily restoring SapphireModelCondition class to evaluate actual adopter usage.

---
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/SapphireModelCondition.java
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * Copyright (c) 2012 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
+package org.eclipse.sapphire.ui;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.sapphire.FilteredListener;
+import org.eclipse.sapphire.Listener;
+import org.eclipse.sapphire.modeling.IModelElement;
+import org.eclipse.sapphire.modeling.PropertyEvent;
+
+/**
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
+
+public abstract class SapphireModelCondition extends SapphireCondition
+{
+    private List<String> dependencies;
+    private Listener listener;
+    
+    protected void initCondition( final ISapphirePart part,
+                                  final String parameter )
+    {
+        this.dependencies = getDependencies();
+        
+        if( this.dependencies != null && ! this.dependencies.isEmpty() )
+        {
+            this.listener = new FilteredListener<PropertyEvent>()
+            {
+                @Override
+                protected void handleTypedEvent( final PropertyEvent event )
+                {
+                    updateConditionState();
+                }
+            };
+            
+            final IModelElement contextModelElement = part.getModelElement();
+            
+            for( String dependency : this.dependencies )
+            {
+                contextModelElement.attach( this.listener, dependency );
+            }
+        }
+    }
+    
+    public List<String> getDependencies()
+    {
+        return Collections.emptyList();
+    }
+    
+    public void dispose()
+    {
+        if( this.listener != null )
+        {
+            final IModelElement contextModelElement = getPart().getModelElement();
+            
+            for( String dependency : this.dependencies )
+            {
+                contextModelElement.detach( this.listener, dependency );
+            }
+        }
+    }
+    
+}
