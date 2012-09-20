70dbd600072e567fe95a62105bb7b49da9202d03
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2012-09-20 08:53:54 -0700

    Enhancement : Comparator for case-insensitive string comparisons
    Java provides API for performing case-insensitive comparison between
    two strings, but often a Comparator implementation for this is required,
    such as when constructing a TreeSet.
    
    Comparators [new]
    {
        static Comparator<String> createIgnoreCaseComparator() [new]
    }
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=390032

---
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/util/Comparators.java
@@ -0,0 +1,50 @@
+/******************************************************************************
+ * Copyright (c) 2012 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
+package org.eclipse.sapphire.util;
+
+import java.util.Comparator;
+
+/**
+ * A collection of common comparators.
+ * 
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
+
+public final class Comparators
+{
+    private static final Comparator<String> IGNORE_CASE_COMPARATOR = new Comparator<String>()
+    {
+        public int compare( final String str1,
+                            final String str2 )
+        {
+            return str1.compareToIgnoreCase( str2 );
+        }
+    };
+    
+    /**
+     * This class is not intended to be instantiated.
+     */
+    
+    private Comparators() {}
+    
+    /**
+     * Creates a case-insensitive string comparator based on String.compareToIgnoreCase( String ) API. 
+     * 
+     * @return the created comparator
+     */
+    
+    public static Comparator<String> createIgnoreCaseComparator()
+    {
+        return IGNORE_CASE_COMPARATOR;
+    }
+    
+}
