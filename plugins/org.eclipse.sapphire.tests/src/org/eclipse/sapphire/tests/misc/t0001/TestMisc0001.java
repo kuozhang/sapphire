b2656a07bb9d727f66a9d0e6ac63c691a5252fcc
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2012-11-26 13:32:02 -0800

    Version change to 0.6.1

--- plugins/org.eclipse.sapphire.tests/src/org/eclipse/sapphire/tests/misc/t0001/TestMisc0001.java
+++ plugins/org.eclipse.sapphire.tests/src/org/eclipse/sapphire/tests/misc/t0001/TestMisc0001.java
@@ -27,7 +27,7 @@ import org.eclipse.sapphire.tests.SapphireTestCase;
 
 public final class TestMisc0001 extends SapphireTestCase
 {
-    private static final String EXPECTED_VERSION_CONSTRAINT = "[0.6-0.6.1)";
+    private static final String EXPECTED_VERSION_CONSTRAINT = "[0.6.1-0.6.2)";
 
     private TestMisc0001( final String name )
     {
