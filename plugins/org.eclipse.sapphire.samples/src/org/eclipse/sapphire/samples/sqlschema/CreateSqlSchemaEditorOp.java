7bec1d5890f2dd31b9e102a290e13da2a0655059
Shenxue Zhou <shenxue.zhou@oracle.com>
2012-11-29 15:48:59 -0700

    Add sql schema editor creation wizard.

---
+++ plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/sqlschema/CreateSqlSchemaEditorOp.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Copyright (c) 2012 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Shenxue Zhou - initial implementation
+ ******************************************************************************/
+
+package org.eclipse.sapphire.samples.sqlschema;
+
+import org.eclipse.sapphire.PreferDefaultValue;
+import org.eclipse.sapphire.modeling.ModelElementType;
+import org.eclipse.sapphire.modeling.ValueProperty;
+import org.eclipse.sapphire.modeling.annotations.DefaultValue;
+import org.eclipse.sapphire.modeling.annotations.GenerateImpl;
+import org.eclipse.sapphire.workspace.CreateWorkspaceFileOp;
+import org.eclipse.sapphire.workspace.WorkspaceFileType;
+
+/**
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
+
+@WorkspaceFileType( Schema.class )
+@GenerateImpl
+
+public interface CreateSqlSchemaEditorOp extends CreateWorkspaceFileOp
+{
+    ModelElementType TYPE = new ModelElementType( CreateSqlSchemaEditorOp.class );
+    
+    // *** FileName ***
+    
+    @DefaultValue( text = "sqlschema.xml" )
+    @PreferDefaultValue
+
+    ValueProperty PROP_FILE_NAME = new ValueProperty( TYPE, CreateWorkspaceFileOp.PROP_FILE_NAME );
+    
+}
