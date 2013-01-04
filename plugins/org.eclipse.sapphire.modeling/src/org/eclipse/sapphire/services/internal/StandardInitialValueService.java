ce88bd7d592953c620ab2fb2d4276edf6d845d6c
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-01-04 13:22:35 -0800

    Releng : Update copyright to 2013

--- plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/internal/StandardInitialValueService.java
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/internal/StandardInitialValueService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Oracle and Accenture
+ * Copyright (c) 2013 Oracle and Accenture
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
