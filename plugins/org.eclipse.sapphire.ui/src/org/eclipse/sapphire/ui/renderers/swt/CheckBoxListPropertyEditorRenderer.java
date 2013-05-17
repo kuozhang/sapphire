442fd8385222adb2fdab74187bbec84d7ee88ca2
Ling Hao <ling.hao@oracle.com>
2013-05-17 09:40:57 -0700

    Fix : CheckBoxListPropertyEditorRenderer needs a listener
    Need to listen to changes in PossibleValuesService.
    
    Bugzilla : https://bugs.eclipse.org/bugs/show_bug.cgi?id=404482

--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/renderers/swt/CheckBoxListPropertyEditorRenderer.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/renderers/swt/CheckBoxListPropertyEditorRenderer.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ *    Ling Hao - [404482] CheckBoxListPropertyEditorRenderer needs a listener
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.renderers.swt;
@@ -90,6 +91,7 @@ import org.eclipse.swt.widgets.TableItem;
 
 /**
  * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ * @author <a href="mailto:ling.hao@oracle.com">Ling Hao</a>
  */
 
 public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRenderer
@@ -98,6 +100,8 @@ public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRender
     private CheckboxTableViewer tableViewer;
     private ElementType memberType;
     private ValueProperty memberProperty;
+    private PossibleValuesService possibleValuesService;
+    private Listener possibleValuesServiceListener;
 
     public CheckBoxListPropertyEditorRenderer( final SapphireRenderingContext context,
                                                final PropertyEditorPart part )
@@ -135,8 +139,19 @@ public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRender
             throw new IllegalStateException();
         }
         
-        final PossibleValuesService possibleValuesService = property.service( PossibleValuesService.class );
+        this.possibleValuesService = property.service( PossibleValuesService.class );
         
+        this.possibleValuesServiceListener = new Listener()
+        {
+            @Override
+            public void handle( final Event event )
+            {
+            	CheckBoxListPropertyEditorRenderer.this.tableViewer.refresh();
+            }
+        };
+        
+        this.possibleValuesService.attach( this.possibleValuesServiceListener );
+
         // Create Controls
         
         final Composite mainComposite = createMainComposite( parent );
@@ -218,7 +233,7 @@ public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRender
                 
                 try
                 {
-                    possibleValues = possibleValuesService.values();
+                    possibleValues = CheckBoxListPropertyEditorRenderer.this.possibleValuesService.values();
                 }
                 catch( Exception e )
                 {
@@ -328,7 +343,7 @@ public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRender
             (
                 this.tableViewer,
                 Collections.<TableColumn,Comparator<Object>>singletonMap( column, comparator ),
-                possibleValuesService.ordered() ? null : column
+                CheckBoxListPropertyEditorRenderer.this.possibleValuesService.ordered() ? null : column
             );
         }
         
@@ -363,6 +378,11 @@ public class CheckBoxListPropertyEditorRenderer extends ListPropertyEditorRender
                 public void run()
                 {
                     selectionService.detach( selectionServiceListener );
+                    
+                    if( CheckBoxListPropertyEditorRenderer.this.possibleValuesService != null )
+                    {
+                    	CheckBoxListPropertyEditorRenderer.this.possibleValuesService.detach( CheckBoxListPropertyEditorRenderer.this.possibleValuesServiceListener );
+                    }
                 }
             }
         );
