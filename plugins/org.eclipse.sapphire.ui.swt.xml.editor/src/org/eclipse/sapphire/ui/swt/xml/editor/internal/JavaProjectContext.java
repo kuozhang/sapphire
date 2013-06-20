d88f870a033810376993f2f5c7c11a45f4294ebc
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-06-20 10:31:56 -0700

    Fix : Imported definitions do not resolve in the sdef editor
    The previous iteration was missing a copyright statement.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=411086

--- plugins/org.eclipse.sapphire.ui.swt.xml.editor/src/org/eclipse/sapphire/ui/swt/xml/editor/internal/JavaProjectContext.java
+++ plugins/org.eclipse.sapphire.ui.swt.xml.editor/src/org/eclipse/sapphire/ui/swt/xml/editor/internal/JavaProjectContext.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2013 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
 package org.eclipse.sapphire.ui.swt.xml.editor.internal;
 
 import java.io.InputStream;
@@ -9,6 +20,10 @@ import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.sapphire.Context;
+
+/**
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
+ */
 
 public final class JavaProjectContext extends Context
 {
