f95fa1d29198415abf9eb549b4f97ba784b3dc00
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-10-14 11:48:32 -0700

    Enhancement : DataService should report context on compute failure
    When the compute method fails in a DataService implementation, the
    developer may have a hard time figuring out the location of the problem.
    DataService should add element type, property and DataService
    implementation class to the exception.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=419392

--- plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/DataService.java
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/services/DataService.java
@@ -13,7 +13,9 @@ package org.eclipse.sapphire.services;
 
 import static org.eclipse.sapphire.modeling.util.MiscUtil.equal;
 
+import org.eclipse.sapphire.ElementType;
 import org.eclipse.sapphire.LocalizableText;
+import org.eclipse.sapphire.PropertyDef;
 import org.eclipse.sapphire.Text;
 
 /**
@@ -24,12 +26,15 @@ public abstract class DataService<T> extends Service
 {
     private static final Object INITIAL_DATA = new Object();
     
-    @Text( "Reentrant call detected during refresh in {0}." )
+    @Text( "Reentrant call detected during refresh in {0}" )
     private static LocalizableText reentrantRefreshMessage;
     
-    @Text( "{0} data accessed prior to service initialization." )
+    @Text( "{0} data accessed prior to service initialization" )
     private static LocalizableText dataAccessedPriorToInitMessage;
-    
+
+    @Text( "{0}: {1}.compute() has failed" )
+    private static LocalizableText computeFailedMessage;
+
     static
     {
         LocalizableText.init( DataService.class );
@@ -83,7 +88,39 @@ public abstract class DataService<T> extends Service
         
         try
         {
-            final T newData = compute();
+            final T newData;
+            
+            try
+            {
+                newData = compute();
+            }
+            catch( Exception e )
+            {
+                final ServiceContext context = context();
+                final String contextLabel;
+                
+                final PropertyDef property = context.find( PropertyDef.class );
+
+                if( property != null )
+                {
+                    contextLabel = property.getModelElementType().getSimpleName() + "." + property.name();
+                }
+                else
+                {
+                    final ElementType type = context.find( ElementType.class );
+                    
+                    if( type != null )
+                    {
+                        contextLabel = type.getSimpleName();
+                    }
+                    else
+                    {
+                        contextLabel = context.getClass().getSimpleName();
+                    }
+                }
+                
+                throw new RuntimeException( computeFailedMessage.format( contextLabel, getClass().getSimpleName() ), e );
+            }
             
             if( this.data == INITIAL_DATA )
             {
