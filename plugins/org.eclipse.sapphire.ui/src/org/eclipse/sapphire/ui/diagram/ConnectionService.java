587127d0cf6fed3246b2ad7a0b0bbd7607159dda
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-11-05 09:30:26 -0800

    Enhancement : Diagram connection establishment should be customizable
    Documentation for ConnectionService, including javadoc, help content
    and a section in the enhancements guide.
    
    Bugzilla : https://bugs.eclipse.org/bugs/show_bug.cgi?id=419509

--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/diagram/ConnectionService.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/diagram/ConnectionService.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Shenxue Zhou - initial implementation and ongoing maintenance
+ *    Konstantin Komissarchik - initial implementation and ongoing maintenance
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.diagram;
@@ -18,30 +19,42 @@ import org.eclipse.sapphire.ui.diagram.editor.DiagramConnectionPart;
 import org.eclipse.sapphire.ui.diagram.editor.DiagramNodePart;
 
 /**
+ * Responsible for listing and establishing connections in a diagram.
+ * 
  * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
+ * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
  */
 
 public abstract class ConnectionService extends Service 
 {
-	/**
-	 * Returns whether it's valid or not to build a connection between two nodes
-	 * 
-	 * @param srcNode Source node
-	 * @param targetNode Target node
-	 * @param connectionType Connection type
-	 * @return True if connection is valid, false otherwise
-	 */
-	public abstract boolean valid(DiagramNodePart srcNode, DiagramNodePart targetNode, String connectionType);
-	
-	/**
-	 * Establish connection between two nodes
-	 * @param srcNode Source node
-	 * @param targetNode Target node
-	 * @param connectionType Connection type
-	 * @return The connection part that has been newly established
-	 */
-	public abstract DiagramConnectionPart connect(DiagramNodePart srcNode, DiagramNodePart targetNode, String connectionType);
-		
-	public abstract List<DiagramConnectionPart> list();
-				
+    /**
+     * Determines whether establishing a connection between the specified nodes is valid.
+     * 
+     * @param node1 the first node
+     * @param node2 the second node
+     * @param connectionType the connection type
+     * @return true if connection is valid and false otherwise
+     */
+    
+    public abstract boolean valid( DiagramNodePart node1, DiagramNodePart node2, String connectionType );
+    
+    /**
+     * Creates a connection between the specified nodes.
+     * 
+     * @param node1 the first node
+     * @param node2 the second node
+     * @param connectionType the connection type
+     * @return the created connection part
+     */
+    
+    public abstract DiagramConnectionPart connect( DiagramNodePart node1, DiagramNodePart node2, String connectionType );
+
+    /**
+     * Lists the existing connections.
+     * 
+     * @return the list of existing connections
+     */
+    
+    public abstract List<DiagramConnectionPart> list();
+
 }
