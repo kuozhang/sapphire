44d2fb8c4ded13c0623fc14819252698edf2d94c
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-11-05 19:22:37 -0800

    Fix : Control context of services registered via annotations
    Added javadoc to @Service annotation to clarify the difference between
    the metamodel and the instance contexts.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=398443

--- plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/modeling/annotations/Service.java
+++ plugins/org.eclipse.sapphire.modeling/src/org/eclipse/sapphire/modeling/annotations/Service.java
@@ -17,6 +17,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Attaches a custom service implementation to an element or a property.
+ * 
  * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
  */
 
@@ -25,20 +27,72 @@ import java.lang.annotation.Target;
 
 public @interface Service
 {
+    /**
+     * The service implementation class.
+     */
+    
     Class<? extends org.eclipse.sapphire.services.Service> impl();
+    
+    /**
+     * The parameters to pass to the service after it is instantiated.
+     */
+    
     Param[] params() default {};
+    
+    /**
+     * The ids of services that should not be active when this service is active.
+     */
+    
     String[] overrides() default {};
+    
+    /**
+     * The context where the service should be activated. Can be either {@link Context.METAMODEL} or {@link Context.INSTANCE}. 
+     * The default is {@link Context.INSTANCE}. 
+     */
+    
     Context context() default Context.INSTANCE;
+    
+    /**
+     * Holds a single service parameter.
+     */
     
     @interface Param
     {
+        /**
+         * Parameter name.
+         */
+        
         String name();
+        
+        /**
+         * Parameter value.
+         */
+        
         String value();
     }
     
+    /**
+     * An enumeration of available service contexts.
+     */
+    
     enum Context
     {
+        /**
+         * Designates the definition of an element or a property. When a service is attached at metamodel context,
+         * all instances share the same copy of the service and the service implementation is not able to access
+         * the instances.  
+         */
+        
         METAMODEL,
+        
+        /**
+         * Designates an instance of an element or a property. When a service is attached at instance context,
+         * each instance has its own copy of the service and the service implementation is able to access
+         * the instance via <nobr><code>context(Element.class)</code></nobr> or <code><nobr>context(Property.class)</nobr></code>
+         * invocation.
+         */
+        
         INSTANCE
     }
+    
 }
