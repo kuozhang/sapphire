468da594451ff0f694ad7bc2a9fbd06f02ed137b
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2013-12-11 10:26:42 -0800

    Fix : ComponentReferenceService does not broadcast changes
    ComponentReferenceService does not have logic to listen for and
    broadcast changes to the reference target.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=423854

--- plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/architecture/internal/ComponentReferenceService.java
+++ plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/architecture/internal/ComponentReferenceService.java
@@ -11,6 +11,9 @@
 
 package org.eclipse.sapphire.samples.architecture.internal;
 
+import org.eclipse.sapphire.FilteredListener;
+import org.eclipse.sapphire.Listener;
+import org.eclipse.sapphire.PropertyContentEvent;
 import org.eclipse.sapphire.samples.architecture.ArchitectureSketch;
 import org.eclipse.sapphire.samples.architecture.Component;
 import org.eclipse.sapphire.services.ReferenceService;
@@ -21,6 +24,25 @@ import org.eclipse.sapphire.services.ReferenceService;
 
 public final class ComponentReferenceService extends ReferenceService
 {
+    private Listener listener;
+    
+    @Override
+    protected void init()
+    {
+        super.init();
+        
+        this.listener = new FilteredListener<PropertyContentEvent>()
+        {
+            @Override
+            protected void handleTypedEvent( final PropertyContentEvent event )
+            {
+                broadcast();
+            }
+        };
+        
+        context( ArchitectureSketch.class ).attach( this.listener, "Components/Name" );
+    }
+
     @Override
     public Object resolve( final String reference ) 
     {
@@ -38,6 +60,14 @@ public final class ComponentReferenceService extends ReferenceService
         }
         
         return null;
+    }
+
+    @Override
+    public void dispose()
+    {
+        context( ArchitectureSketch.class ).detach( this.listener, "Components/Name" );
+        
+        super.dispose();
     }
 
 }
