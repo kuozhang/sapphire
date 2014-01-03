540a789c94ecf4c1550f91ad6d955d126fa342c1
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2014-01-03 09:32:33 -0800

    Enhancement : Label improvements for the separator part
    In the sdef editor, the separator part is presented as "line separator".
    The "line" qualification is not necessary as there are no other kind of
    separators. Further, when a separator has a label, sdef editor should
    use that label in the content outline instead of the generic "separator"
    label.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=424859

--- plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/forms/LineSeparatorDef.java
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/forms/LineSeparatorDef.java
@@ -23,7 +23,7 @@ import org.eclipse.sapphire.modeling.xml.annotations.XmlBinding;
  * @author <a href="mailto:konstantin.komissarchik@oracle.com">Konstantin Komissarchik</a>
  */
 
-@Label( standard = "line separator" )
+@Label( standard = "separator" )
 @Image( path = "LineSeparatorDef.png" )
 @XmlBinding( path = "separator" )
 
