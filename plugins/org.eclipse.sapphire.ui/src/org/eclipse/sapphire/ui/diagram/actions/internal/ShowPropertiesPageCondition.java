8f08183c977e916d357ac238acd9fa32f82d84eb
Shenxue Zhou <shenxue.zhou@oracle.com>
2014-09-03 14:56:42 -0600

    Enhancement : Add "Show properties page" action to the diagram editor
    Action handler implemented with a condition where the action shows only
    when the selected part(diagram, node, shape or connection) has
    properties page.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=443246

---
+++ plugins/org.eclipse.sapphire.ui/src/org/eclipse/sapphire/ui/diagram/actions/internal/ShowPropertiesPageCondition.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright (c) 2014 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Shenxue Zhou - initial implementation and ongoing maintenance
+ ******************************************************************************/
+
+package org.eclipse.sapphire.ui.diagram.actions.internal;
+
+import org.eclipse.sapphire.ui.SapphireCondition;
+import org.eclipse.sapphire.ui.forms.PropertiesViewContributorPart;
+
+/**
+ * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
+ */
+
+public class ShowPropertiesPageCondition extends SapphireCondition 
+{
+
+	@Override
+	protected boolean evaluate() 
+	{
+    	if (getPart() instanceof PropertiesViewContributorPart)
+    	{
+    		if (((PropertiesViewContributorPart)getPart()).getPropertiesViewContribution() != null)
+    		{
+    			return true;
+    		}
+    	}
+        return false;
+	}
+
+}
