3279d4a7d54f52e96613105dae09eb4a4ba48f30
Shenxue Zhou <shenxue.zhou@oracle.com>
2014-10-15 15:29:20 -0600

    Fix : Bendpoint can be created under a node
    Overides Command's canExecute() method in CreateBendpointCommand and
    MoveBendpointCommand to detect whether the bendpoint is on a node.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=445831

--- plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/commands/CreateBendPointCommand.java
+++ plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/commands/CreateBendPointCommand.java
@@ -7,20 +7,42 @@
  *
  * Contributors:
  *    Ling Hao - initial implementation and ongoing maintenance
+ *    Shenxue Zhou - Bug 445831 - Bendpoint can be created under a node
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.swt.gef.commands;
 
+import java.util.List;
+
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.sapphire.ui.swt.gef.model.DiagramNodeModel;
+
 /**
  * @author <a href="mailto:ling.hao@oracle.com">Ling Hao</a>
+ * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
  */
 
-public class CreateBendPointCommand extends BendPointCommand {
-
-	public void execute() {
+public class CreateBendPointCommand extends BendPointCommand 
+{
+	@Override
+	public boolean canExecute() 
+	{
+		List<DiagramNodeModel> nodes = getDiagramConnectionModel().getDiagramModel().getNodes();
+		Point location = getLocation();
+		for (DiagramNodeModel node : nodes)
+		{
+			if (pointInBounds(location, node.getShapePresentation().getFigure().getBounds()))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void execute() 
+	{
 		getDiagramConnectionModel().getModelPart().addBendpoint(getIndex(), getLocation().x, getLocation().y);
 		super.execute();
 	}
-
 
 }
--- plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/commands/MoveBendPointCommand.java
+++ plugins/org.eclipse.sapphire.ui.swt.gef/src/org/eclipse/sapphire/ui/swt/gef/commands/MoveBendPointCommand.java
@@ -7,19 +7,59 @@
  *
  * Contributors:
  *    Ling Hao - initial implementation and ongoing maintenance
+ *    Shenxue Zhou - Bug 445831 - Bendpoint can be created under a node
  ******************************************************************************/
 
 package org.eclipse.sapphire.ui.swt.gef.commands;
 
+import java.util.List;
+
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.sapphire.ui.Bounds;
+import org.eclipse.sapphire.ui.swt.gef.model.DiagramNodeModel;
+
 /**
  * @author <a href="mailto:ling.hao@oracle.com">Ling Hao</a>
+ * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
  */
 
-public class MoveBendPointCommand extends BendPointCommand {
+public class MoveBendPointCommand extends BendPointCommand 
+{
 
-	public void execute() {
+	@Override 
+	public boolean canExecute()
+	{
+		List<DiagramNodeModel> nodes = getDiagramConnectionModel().getDiagramModel().getNodes();
+		Point location = getLocation();
+		for (DiagramNodeModel node : nodes)
+		{
+			if (pointInBounds(location, node.getShapePresentation().getFigure().getBounds()))
+			{
+				return false;
+			}
+		}
+		return true;		
+	}
+	
+	public void execute() 
+	{
 		getDiagramConnectionModel().getModelPart().updateBendpoint(getIndex(), getLocation().x, getLocation().y);
 		super.execute();
 	}
+	
+	protected boolean pointInBounds(Point p, Bounds bounds)
+	{
+		int x = bounds.getX();
+		int y = bounds.getY();
+		int x2 = x + bounds.getWidth();
+		int y2 = y + bounds.getHeight();
+		
+		if (x <= p.x && p.x <= x2 && y <= p.y && p.y <= y2)
+		{
+			return true;
+		}
+		return false;
+	}
+	
 
 }
