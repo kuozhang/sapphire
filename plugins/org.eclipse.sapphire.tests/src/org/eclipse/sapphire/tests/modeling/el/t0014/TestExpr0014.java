61f94120c1d52e1b1e9aa48e48d7fae87cbd3c5d
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2014-12-18 19:55:25 -0800

    Releng : Change version to 8.2

--- plugins/org.eclipse.sapphire.tests/src/org/eclipse/sapphire/tests/modeling/el/t0014/TestExpr0014.java
+++ plugins/org.eclipse.sapphire.tests/src/org/eclipse/sapphire/tests/modeling/el/t0014/TestExpr0014.java
@@ -40,7 +40,7 @@ public final class TestExpr0014 extends TestExpr
     {
         final FunctionContext context = new FunctionContext();
         
-        testForExpectedValue( context, "${ SapphireVersionMatches( '[8.1.1-8.1.2)' ) }", Boolean.TRUE );
+        testForExpectedValue( context, "${ SapphireVersionMatches( '[8.2-8.2.1)' ) }", Boolean.TRUE );
         testForExpectedValue( context, "${ SapphireVersionMatches( '[0.5-0.6)' ) }", Boolean.FALSE );
     }
 
