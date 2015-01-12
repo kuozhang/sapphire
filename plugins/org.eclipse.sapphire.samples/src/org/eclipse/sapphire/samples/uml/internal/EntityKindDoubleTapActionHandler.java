286e06ea6090b0e3f69fd52030035226374516fe
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2015-01-12 13:25:30 -0800

    Releng : Change copyright year to 2015

--- plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/uml/internal/EntityKindDoubleTapActionHandler.java
+++ plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/uml/internal/EntityKindDoubleTapActionHandler.java
@@ -1,7 +1,7 @@
 package org.eclipse.sapphire.samples.uml.internal;
 
 /******************************************************************************
- * Copyright (c) 2014 Oracle
+ * Copyright (c) 2015 Oracle
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
