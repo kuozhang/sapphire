97f284ff13325ee4e9537a2ce014f8971cca72d2
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2015-01-12 13:10:35 -0800

    Releng : Change copyright year to 2015

--- plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/OpenSapphireWebSiteActionHandler.java
+++ plugins/org.eclipse.sapphire.samples/src/org/eclipse/sapphire/samples/OpenSapphireWebSiteActionHandler.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2014 Liferay, Inc.
+ * Copyright (c) 2015 Liferay, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
