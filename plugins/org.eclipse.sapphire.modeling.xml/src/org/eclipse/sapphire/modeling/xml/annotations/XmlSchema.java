2f0fc14772c5cb10ed213711ed7c0138bc829241
Konstantin Komissarchik <konstantin.komissarchik@oracle.com>
2015-01-15 15:05:05 -0800

    Enhancement : @XmlSchema annotation should be repeatable
    Taking advantage of Java 8, @XmlSchema annotation should be made
    repeatable, saving user the trouble of manually wrapping @XmlSchemas
    container annotation around multiple @XmlSchema annotations present at
    a single site.
    
    Bugzilla: https://bugs.eclipse.org/bugs/show_bug.cgi?id=457651

--- plugins/org.eclipse.sapphire.modeling.xml/src/org/eclipse/sapphire/modeling/xml/annotations/XmlSchema.java
+++ plugins/org.eclipse.sapphire.modeling.xml/src/org/eclipse/sapphire/modeling/xml/annotations/XmlSchema.java
@@ -13,6 +13,7 @@
 package org.eclipse.sapphire.modeling.xml.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -26,6 +27,7 @@ import java.lang.annotation.Target;
 
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.TYPE )
+@Repeatable( XmlSchemas.class )
 
 public @interface XmlSchema 
 {
